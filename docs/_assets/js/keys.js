f0d2b1405f49e30f3a2ff1deee369bd85966d5b3
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2020-12-24 22:51:23 +0100

    store

---
+++ docs/_assets/js/keys.js
@@ -0,0 +1,5 @@
+export default {
+    pubnub_pub: "pub-c-6ff16a5e-7b35-4001-ae58-815828398daf",
+    pubnub_subscribe: "sub-c-d0d2b5d4-45e0-11eb-a73a-1eec528e8f1f",
+    pubnub_secret: "sec-c-ZjdmYjY3NDQtODg4Mi00ZGExLTllZjktMWY1YmUyNGY3ZTA5"
+}
