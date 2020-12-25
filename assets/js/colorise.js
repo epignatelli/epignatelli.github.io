3ca1863512312b94f422d7d121dcc436b461e012
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2020-12-25 21:54:57 +0100

    having fun

--- assets/js/colorise.js
+++ assets/js/colorise.js
@@ -1,19 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var elements = document.getElementById('title');
-    console.log(elements)
-    var s_min = 20;
-    var s_max= 70;
-    var l_min = 30;
-    var l_max = 90;
-
+    var elements = document.getElementsByTagName('div');
     for (var i = 0; i < elements.length; i++) {
         elements[i].onmouseover = function(e) {
-            var h = Math.floor(Math.random()*360)
-            var s = Math.max([Math.min([Math.random() * 100, s_min]), s_max])
-            var l = Math.max([Math.min([Math.random() * 100, l_min]), l_max])
-            var color = "hsl(" + h + "," + s + "%, " + l + "%) !important"
-            console.log(h, s, l, color)
-            this.style['color'] = color;
+            var h = Math.random() * 360 + 1;
+            var s = Math.floor(Math.random() * 50 + 51);
+            var l = Math.floor(Math.random() * 50 + 51);
+            var color = 'hsl(' + h + ',' + s + '%,' + l + '%)';
+            console.log(color);
+            this.style['background'] = color;
         }
     }
 });
