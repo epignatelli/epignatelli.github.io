ecc7421b3f3a45f0dbd22e79b9c16c07bbd50c17
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2020-12-25 22:27:30 +0100

    add a way to reset the mess

--- assets/js/colorise.js
+++ assets/js/colorise.js
@@ -1,5 +1,26 @@
+function allElements() {
+    return document.getElementsByTagName('div');
+}
+
+function attribute() {
+    return "background"
+}
+
+function defaultAtt() {
+    return "white";
+}
+
+
+document.addEventListener("keydown", function() {
+    var elements = allElements();
+    for (var i = 0; i < elements.length; i++) {
+        var att = attribute();
+        elements[i].style[att] = defaultAtt();
+    }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
-    var elements = document.getElementsByTagName('div');
+    var elements = allElements();
     for (var i = 0; i < elements.length; i++) {
         elements[i].onmouseover = function(e) {
             var h = Math.random() * 360 + 1;
@@ -7,7 +28,16 @@ document.addEventListener("DOMContentLoaded", function() {
             var l = Math.floor(Math.random() * 50 + 51);
             var color = 'hsl(' + h + ',' + s + '%,' + l + '%)';
             console.log(color);
-            this.style['background'] = color;
+            var att = attribute();
+            this.style[att] = color;
+        }
+
+        elements[i].onmousedown = function(e) {
+            var elements = allElements();
+            for (var i = 0; i < elements.length; i++) {
+                var att = attribute();
+                elements[i].style[att] = defaultAtt();
+            }
         }
     }
 });
